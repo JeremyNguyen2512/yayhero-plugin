efd398b3a96314555b7fcd85e23373cdf7f4c244
Hoang Truong <tvhoang91@gmail.com>
2023-05-09 17:24:13 +0700

    format php

--- includes/classes/class-check-user-role-permission.php
+++ includes/classes/class-check-user-role-permission.php
@@ -1,17 +1,19 @@
 <?php
+
 /**
  * This file will check user role permission 
  */
 
- if(!class_exists('WP_Check_User_Role_Permission')){
-    class WP_Check_User_Role_Permission{
+if (!class_exists('WP_Check_User_Role_Permission')) {
+    class WP_Check_User_Role_Permission
+    {
         public function __construct()
         {
-            $user_permission = current_user_can('manage_options')?'write':'read';
+            $user_permission = current_user_can('manage_options') ? 'write' : 'read';
             wp_localize_script("module/yayhero/main.tsx", 'appLocalize', [
                 'user_permission'    => $user_permission,
             ]);
         }
     }
     new WP_Check_User_Role_Permission();
- }
+}
--- includes/classes/class-create-custom-post-type.php
+++ includes/classes/class-create-custom-post-type.php
@@ -1,40 +1,43 @@
 <?php
+
 /**
  * This file will create custom post type
  */
 
- if(!class_exists('WP_Create_Custom_Post_Type')){
-    class WP_Create_Custom_Post_Type{
+if (!class_exists('WP_Create_Custom_Post_Type')) {
+	class WP_Create_Custom_Post_Type
+	{
 
-        public function __construct()
-        {
-            add_action('init', [$this,'register_yay_hero']);
-        }
+		public function __construct()
+		{
+			add_action('init', [$this, 'register_yay_hero']);
+		}
 
-        public function register_yay_hero(){
+		public function register_yay_hero()
+		{
 
 			$labels = array(
-				'name'                  => _x( 'Yay Hero', 'Post Type General Name', 'yayhero' ),
-				'singular_name'         => _x( 'Yay Hero', 'Post Type Singular Name', 'yayhero' ),
-				'menu_name'             => __( 'Yay Hero', 'yayhero' ),
-				'name_admin_bar'        => __( 'Yay Hero', 'yayhero' ),
-				'archives'              => __( 'Item Archives', 'yayhero' ),
-				'attributes'            => __( 'Item Attributes', 'yayhero' ),
-				'parent_item_colon'     => __( 'Parent Item:', 'yayhero' ),
-				'all_items'             => __( 'All Items', 'yayhero' ),
-				'add_new_item'          => __( 'Add New Item', 'yayhero' ),
-				'add_new'               => __( 'Add New', 'yayhero' ),
-				'new_item'              => __( 'New Item', 'yayhero' ),
-				'edit_item'             => __( 'Edit Item', 'yayhero' ),
-				'update_item'           => __( 'Update Item', 'yayhero' ),
-				'view_item'             => __( 'View Item', 'yayhero' ),
-				'view_items'            => __( 'View Items', 'yayhero' ),
+				'name'                  => _x('Yay Hero', 'Post Type General Name', 'yayhero'),
+				'singular_name'         => _x('Yay Hero', 'Post Type Singular Name', 'yayhero'),
+				'menu_name'             => __('Yay Hero', 'yayhero'),
+				'name_admin_bar'        => __('Yay Hero', 'yayhero'),
+				'archives'              => __('Item Archives', 'yayhero'),
+				'attributes'            => __('Item Attributes', 'yayhero'),
+				'parent_item_colon'     => __('Parent Item:', 'yayhero'),
+				'all_items'             => __('All Items', 'yayhero'),
+				'add_new_item'          => __('Add New Item', 'yayhero'),
+				'add_new'               => __('Add New', 'yayhero'),
+				'new_item'              => __('New Item', 'yayhero'),
+				'edit_item'             => __('Edit Item', 'yayhero'),
+				'update_item'           => __('Update Item', 'yayhero'),
+				'view_item'             => __('View Item', 'yayhero'),
+				'view_items'            => __('View Items', 'yayhero'),
 			);
 			$args = array(
-				'label'                 => __( 'Yay Hero', 'yayhero' ),
-				'description'           => __( 'Post Type Description', 'yayhero' ),
+				'label'                 => __('Yay Hero', 'yayhero'),
+				'description'           => __('Post Type Description', 'yayhero'),
 				'labels'                => $labels,
-				'supports'              => array( 'title' ),
+				'supports'              => array('title'),
 				'hierarchical'          => false,
 				'public'                => true,
 				'show_ui'               => false,
@@ -48,9 +51,8 @@
 				'publicly_queryable'    => true,
 				'capability_type'       => 'page',
 			);
-			register_post_type( 'yay_hero', $args );
-        }
-    }
- new  WP_Create_Custom_Post_Type();
-
- }
+			register_post_type('yay_hero', $args);
+		}
+	}
+	new  WP_Create_Custom_Post_Type();
+}
