d9197b6875b362c041da6d7bad2d7d2ef8a6728f
Jeremy Nguyen <jeremy@yaycommerce.com>
2023-05-12 08:53:57 +0700

    Done Icon Loading

--- apps/heroes/src/App.tsx
+++ apps/heroes/src/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import ListHero from './routes/ListHero';
 
 const queryClient = new QueryClient();
 function App() {
